16564d234ecc39b69fd06bfdf749443d213d4bbf
bushmrt1 <randy.bushman@jhuapl.edu>
2023-05-23 20:54:13 -0400

    Added mnist dense network. (Untested).

--- test.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-from tensorflow.python.client import device_lib
-
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-print(device_lib.list_local_devices())
